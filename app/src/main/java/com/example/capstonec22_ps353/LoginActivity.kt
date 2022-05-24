3c14382fb00621241a3e63dd7b221e19ebedb168
debbyagestira <debbyagestira@gmail.com>
2022-05-24 21:45:25 +0700

    new LoginActivity.kt

---
+++ app/src/main/java/com/example/capstonec22_ps353/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonec22_ps353
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//ini Debby yang commit LoginActivity
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
