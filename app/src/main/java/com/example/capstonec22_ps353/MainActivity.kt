d7d22c14d5cbfe625fbccb118ef1c0282427e605
fadlanam30 <fadlann420@gmail.com>
2022-05-24 21:37:17 +0700

    init

---
+++ app/src/main/java/com/example/capstonec22_ps353/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.capstonec22_ps353
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
