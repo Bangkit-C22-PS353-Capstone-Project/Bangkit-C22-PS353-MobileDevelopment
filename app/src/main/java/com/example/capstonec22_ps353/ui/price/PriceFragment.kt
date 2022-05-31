24247907e1400150eece89a1cfc3805983e50cac
fadlanam30 <fadlann420@gmail.com>
2022-05-31 19:56:27 +0700

    init commit fadlan

--- app/src/main/java/com/example/capstonec22_ps353/ui/price/PriceFragment.kt
+++ app/src/main/java/com/example/capstonec22_ps353/ui/price/PriceFragment.kt
@@ -1,28 +1,67 @@
 package com.example.capstonec22_ps353.ui.price
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.capstonec22_ps353.R
+import com.example.capstonec22_ps353.databinding.FragmentLoginBinding
+import com.example.capstonec22_ps353.databinding.FragmentPriceBinding
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class PriceFragment : Fragment() {
 
+    private lateinit var lineDataSet: LineDataSet
+    private lateinit var lineList: ArrayList<Entry>
+    private lateinit var lineData: LineData
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentPriceBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentPriceBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_price, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lineList = ArrayList()
+        lineList.add(Entry(10f, 100f))
+        lineList.add(Entry(20f, 300f))
+        lineList.add(Entry(30f, 200f))
+        lineList.add(Entry(40f, 600f))
+        lineList.add(Entry(50f, 500f))
+
+        lineDataSet = LineDataSet(lineList, "Count")
+        lineData = LineData(lineDataSet)
+        binding.lineChart.data = lineData
+        lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS)
+        lineDataSet.valueTextColor = Color.BLUE
+        lineDataSet.valueTextSize = 15f
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
+
+private fun LineDataSet.setColors(joyfulColors: IntArray?) {
 
 }
