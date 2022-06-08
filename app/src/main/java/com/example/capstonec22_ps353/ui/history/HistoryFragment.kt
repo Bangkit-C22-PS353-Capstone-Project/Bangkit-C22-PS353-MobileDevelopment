531dfbb8e06c1ab4f1f339a8826833b26e73d277
debbyagestira <debbyagestira@gmail.com>
2022-06-09 02:48:14 +0700

    update adapter and fragment ListHistory

--- app/src/main/java/com/example/capstonec22_ps353/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/capstonec22_ps353/ui/history/HistoryFragment.kt
@@ -7,16 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.capstonec22_ps353.databinding.FragmentHistoryBinding
+import com.example.capstonec22_ps353.ui.adapter.ListHistoryAdapter
 import com.example.capstonec22_ps353.utils.SharedViewModel
 import kotlinx.coroutines.launch
 
 class HistoryFragment : Fragment() {
 
+    private lateinit var rvHistory: RecyclerView
+
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
     private var _binding : FragmentHistoryBinding?=null
     private val binding get() = _binding!!
+
+    private lateinit var listHistoryAdapter: ListHistoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +38,7 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupActionButton()
+        showRecyclerList()
     }
 
     private fun setupActionButton() {
@@ -39,6 +47,12 @@ class HistoryFragment : Fragment() {
 
             }
         }
+    }
+
+    private fun showRecyclerList() {
+        rvHistory.layoutManager = LinearLayoutManager(context)
+        rvHistory.setHasFixedSize(true)
+        rvHistory.adapter = listHistoryAdapter
     }
 
     override fun onDestroy() {
